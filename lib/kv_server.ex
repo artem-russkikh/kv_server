e2d5f651bceabd99d8a91665bde07ee1bbc4e890
Artem Russkikh <rusartx@gmail.com>
2016-09-16 15:29:08 +0500

    Move start to begin

--- lib/kv_server.ex
+++ lib/kv_server.ex
@@ -1,16 +1,24 @@
 defmodule KVServer do
   use Application
-
   require Logger
 
+  @doc false
+  def start(_type, _args) do
+    import Supervisor.Spec
+
+    children = [
+      supervisor(Task.Supervisor, [[name: KVServer.TaskSupervisor]]),
+      worker(Task, [KVServer, :accept, [4040]])
+    ]
+
+    opts = [strategy: :one_for_one, name: KVServer.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  @doc """
+  Starts accepting connections on the given `port`.
+  """
   def accept(port) do
-    # The options below mean:
-    #
-    # 1. `:binary` - receives data as binaries (instead of lists)
-    # 2. `packet: :line` - receives data line by line
-    # 3. `active: false` - blocks on `:gen_tcp.recv/2` until data is available
-    # 4. `reuseaddr: true` - allows us to reuse the address if the listener crashes
-    #
     {:ok, socket} = :gen_tcp.listen(port,
                       [:binary, packet: :line, active: false, reuseaddr: true])
     Logger.info "Accepting connections on port #{port}"
@@ -39,24 +47,5 @@ defmodule KVServer do
 
   defp write_line(line, socket) do
     :gen_tcp.send(socket, line)
-  end
-
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
-  def start(_type, _args) do
-    import Supervisor.Spec
-
-    # Define workers and child supervisors to be supervised
-    children = [
-      # Starts a worker by calling: KVServer.Worker.start_link(arg1, arg2, arg3)
-      # worker(KVServer.Worker, [arg1, arg2, arg3]),
-      supervisor(Task.Supervisor, [[name: KVServer.TaskSupervisor]]),
-      worker(Task, [KVServer, :accept, [4040]])
-    ]
-
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: KVServer.Supervisor]
-    Supervisor.start_link(children, opts)
   end
 end
