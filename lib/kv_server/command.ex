03cc997b3c5c7f8563afc76a997689e696df7142
Artem Russkikh <rusartx@gmail.com>
2016-09-16 15:53:19 +0500

    Recognize commands

--- lib/kv_server/command.ex
+++ lib/kv_server/command.ex
@@ -7,8 +7,42 @@ defmodule KVServer.Command do
       iex> KVServer.Command.parse "CREATE shopping\r\n"
       {:ok, {:create, "shopping"}}
 
+      iex> KVServer.Command.parse "CREATE  shopping  \r\n"
+      {:ok, {:create, "shopping"}}
+
+      iex> KVServer.Command.parse "PUT shopping milk 1\r\n"
+      {:ok, {:put, "shopping", "milk", "1"}}
+
+      iex> KVServer.Command.parse "GET shopping milk\r\n"
+      {:ok, {:get, "shopping", "milk"}}
+
+      iex> KVServer.Command.parse "DELETE shopping eggs\r\n"
+      {:ok, {:delete, "shopping", "eggs"}}
+
+  Unknown commands or commands with the wrong number of
+  arguments return an error:
+
+      iex> KVServer.Command.parse "UNKNOWN shopping eggs\r\n"
+      {:error, :unknown_command}
+
+      iex> KVServer.Command.parse "GET shopping\r\n"
+      {:error, :unknown_command}
+
   """
   def parse(line) do
-    :not_implemented
+    case String.split(line) do
+      ["CREATE", bucket] -> {:ok, {:create, bucket}}
+      ["PUT", bucket, key, value] -> {:ok, {:put, bucket, key, value}}
+      ["GET", bucket, key] -> {:ok, {:get, bucket, key}}
+      ["DELETE", bucket, key] -> {:ok, {:delete, bucket, key}}
+      _ -> {:error, :unknown_command}
+    end
+  end
+
+  @doc """
+  Runs the given command.
+  """
+  def run(command) do
+    {:ok, "OK\r\n"}
   end
 end
