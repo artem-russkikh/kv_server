4b4c0fc540bb990ed9b284f399101259bf4bf50e
Artem Russkikh <rusartx@gmail.com>
2016-09-16 15:58:06 +0500

    Use with

--- lib/kv_server.ex
+++ lib/kv_server.ex
@@ -34,17 +34,9 @@ defmodule KVServer do
 
   defp serve(socket) do
     msg =
-      case read_line(socket) do
-        {:ok, data} ->
-          case KVServer.Command.parse(data) do
-            {:ok, command} ->
-              KVServer.Command.run(command)
-            {:error, _} = err ->
-              err
-          end
-        {:error, _} = err ->
-          err
-      end
+      with {:ok, data} <- read_line(socket),
+           {:ok, command} <- KVServer.Command.parse(data),
+           do: KVServer.Command.run(command)
 
     write_line(socket, msg)
     serve(socket)
