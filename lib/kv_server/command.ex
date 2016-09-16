6bb93d74bd5985ba6678a66194b27195374850a8
Artem Russkikh <rusartx@gmail.com>
2016-09-16 15:59:55 +0500

    Define run functions

--- lib/kv_server/command.ex
+++ lib/kv_server/command.ex
@@ -42,7 +42,38 @@ defmodule KVServer.Command do
   @doc """
   Runs the given command.
   """
-  def run(command) do
+  def run(command)
+
+  def run({:create, bucket}) do
+    KV.Registry.create(KV.Registry, bucket)
     {:ok, "OK\r\n"}
+  end
+
+  def run({:get, bucket, key}) do
+    lookup bucket, fn pid ->
+      value = KV.Bucket.get(pid, key)
+      {:ok, "#{value}\r\nOK\r\n"}
+    end
+  end
+
+  def run({:put, bucket, key, value}) do
+    lookup bucket, fn pid ->
+      KV.Bucket.put(pid, key, value)
+      {:ok, "OK\r\n"}
+    end
+  end
+
+  def run({:delete, bucket, key}) do
+    lookup bucket, fn pid ->
+      KV.Bucket.delete(pid, key)
+      {:ok, "OK\r\n"}
+    end
+  end
+
+  defp lookup(bucket, callback) do
+    case KV.Registry.lookup(KV.Registry, bucket) do
+      {:ok, pid} -> callback.(pid)
+      :error -> {:error, :not_found}
+    end
   end
 end
